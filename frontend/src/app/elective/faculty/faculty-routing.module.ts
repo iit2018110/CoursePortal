80196316736b1f94c40a5c584549d6d3eca700cc
Vikash Kumar <vikash.kumar@docquity.com>
2022-03-29 02:20:29 +0530

    subject preference of faculty donegit status

--- frontend/src/app/elective/faculty/faculty-routing.module.ts
+++ frontend/src/app/elective/faculty/faculty-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { FacultyComponent } from './faculty.component';
+import { GetStudentsComponent } from './get-students/get-students.component';
 import { LoginComponent } from './login/login.component';
+import { SubjectPreferencesComponent } from './subject-preferences/subject-preferences.component';
 
 const routes: Routes = [{ path: '', redirectTo: 'login' },
                         { path: 'login', component: LoginComponent } ,
-                        { path: 'dashboard', component: DashboardComponent } 
+                        { path: 'dashboard', component: DashboardComponent },
+                        { path: 'subject_preference', component: SubjectPreferencesComponent},
+                        { path: 'get_students', component: GetStudentsComponent } 
                       ];
 
 @NgModule({
