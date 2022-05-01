df7bc4c392a990e92d1aafb0e7481865c209fed9
ipir140 <prabalcs@gmail.com>
2022-05-01 22:06:02 +0530

    Added Reset in elective, project & core for admin

--- backend/controllers/admin/elective/reset.js
+++ backend/controllers/admin/elective/reset.js
@@ -6,8 +6,10 @@ module.exports.reset_elective = async (req, res) => {
     
     await sequelize.query(`truncate table course_students;`)
     await sequelize.query(`truncate table course_faculties;`)
+    await sequelize.query(`truncate table buffer_courses_cc;`)
     await sequelize.query(`truncate table buffer_course_students;`)
     await sequelize.query(`truncate table buffer_course_faculty_hod;`)
+    await sequelize.query(`truncate table buffer_basket_students;`)
     await sequelize.query(`truncate table buffer_baskets_hod;`)
     await sequelize.query(`truncate table faculty_preferences;`)
     await sequelize.query(`truncate table running_courses;`)
